3da547e8141d7e2e5736e5a8d62adabed7a84f1b
Vincentyun <vincentzyc@qq.com>
2019-01-28 10:31:13 +0800

    personal-tax

--- src/views/personal-tax.vue
+++ src/views/personal-tax.vue
@@ -1,37 +1,64 @@
 <template>
   <basePage title="personalTax">
     <div class="personal-tax">
-      <h1>This is an about page</h1>
-      <h1>Hello world</h1>
-      <cube-button :primary="true" @click="$router.back()">back</cube-button>
-      <cube-button :primary="true" @click="test()">test</cube-button>
+      <div class="input-item">
+        <label>工资</label>
+        <input type="number" v-model.number="wages" placeholder="请输入工资">
+      </div>
+      <div class="input-item">
+        <label>社保</label>
+        <input type="number" v-model.number="socialSecurity" placeholder="请输入社保">
+      </div>
+      <div class="input-item">
+        <label>公积金</label>
+        <input type="number" v-model.number="AccumulationFund" placeholder="请输入公积金">
+      </div>
+      <div class="input-item">
+        <label>扣税金额</label>
+        <input type="number" v-model="taxDeduction" readonly>
+      </div>
+      <div class="input-item">
+        <label>税后</label>
+        <input type="number" v-model="result" readonly>
+      </div>
     </div>
+    <TaxRateTable/>
+    <cube-button :primary="true" @click="computed()" class="mg-t20">计算</cube-button>
   </basePage>
 </template>
 
 <script>
+import TaxRateTable from './tax-rate-table.vue';
 export default {
+  components: {
+    TaxRateTable
+  },
+  data() {
+    return {
+      wages: 5000,
+      socialSecurity: 380.34,
+      AccumulationFund: 105,
+      result: "",
+      taxDeduction: ""
+    }
+  },
   methods: {
-    test() {
-      this.$loading.open('123');
-      setTimeout(() => {
-        this.$loading.close();
-      }, 3000);
+    computedWages(amount) {
+      if (amount < 0) return 0;
+      if (amount <= 3000) return amount * 0.03;
+      if (amount > 3000 && amount <= 12000) return amount * 0.1 - 210;
+      if (amount > 12000 && amount <= 25000) return amount * 0.2 - 1410;
+      if (amount > 25000 && amount <= 35000) return amount * 0.25 - 2660;
+      if (amount > 35000 && amount <= 55000) return amount * 0.3 - 4410;
+      if (amount > 55000 && amount <= 80000) return amount * 0.35 - 7160;
+      if (amount > 80000) return amount * 0.45 - 15160;
+    },
+    computed() {
+      let amount = this.wages - this.socialSecurity - this.AccumulationFund - 5000;
+      this.taxDeduction = this.computedWages(amount).toFixed(2);
+      this.result = amount + 5000 - this.taxDeduction;
     }
   }
 }
-// import { Component, Vue } from "vue-property-decorator";
-// import HelloWorld from "@/components/hello-world.vue"; // @ is an alias to /src
-
-// @Component({
-//   components: {
-//     HelloWorld
-//   }
-// })
-// export default class Home extends Vue {
-//   protected test() {
-//     this.$loading.open('123')
-//   }
-// }
 </script>
 
---
+++ src/views/tax-rate-table.vue
@@ -0,0 +1,62 @@
+<template>
+  <table>
+    <tbody>
+      <tr>
+        <td>应纳税所得额(X)</td>
+        <td>税率</td>
+        <td>速算扣除数</td>
+      </tr>
+      <tr>
+        <td>(0＜X≤3,000)</td>
+        <td>3%</td>
+        <td>0</td>
+      </tr>
+      <tr>
+        <td>(3,000＜X≤12,000)</td>
+        <td>10%</td>
+        <td>210</td>
+      </tr>
+      <tr>
+        <td>(12,000＜X≤25,000)</td>
+        <td>20%</td>
+        <td>1,410</td>
+      </tr>
+      <tr>
+        <td>(25,000＜X≤35,000)</td>
+        <td>25%</td>
+        <td>2,660</td>
+      </tr>
+      <tr>
+        <td>(35,000＜X≤55,000)</td>
+        <td>30%</td>
+        <td>4,410</td>
+      </tr>
+      <tr>
+        <td>(55,000＜X≤80,000)</td>
+        <td>35%</td>
+        <td>7,160</td>
+      </tr>
+      <tr>
+        <td>(X＞80,000)</td>
+        <td>45%</td>
+        <td>15,160</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style lang="stylus" scoped>
+table, caption, tbody, tfoot, thead, tr, th, td {
+  border: 1px solid #666;
+  padding: 5px;
+}
+
+table {
+  width 100%;
+  margin-top  10px
+  text-align: center;
+  border-collapse: collapse;
+}
+</style>
+
+
