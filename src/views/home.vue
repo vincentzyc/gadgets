2763b6988b73d5a7f1f4e6494a6a64849c4db90e
Vincentyun <vincentzyc@qq.com>
2019-02-12 17:02:36 +0800

    设置文件名大小写敏感

---
+++ src/views/home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="home">
+    <ul class="card-list">
+      <router-link tag="li" class="card-item" to="/personalTax">个税计算器</router-link>
+      <!-- <router-link tag="li" class="card-item" to="/helloWorld">helloWord</router-link> -->
+    </ul>
+  </div>
+</template>
