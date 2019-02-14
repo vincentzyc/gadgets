d83d998449d99e4ea5ea4041bb073e03ac802cfa
Vincentyun <vincentzyc@qq.com>
2019-02-14 10:58:14 +0800

    add home.vue

---
+++ src/views/home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home">
+    <ul class="card-list">
+      <router-link tag="li" class="card-item" to="/personalTax">个税计算器</router-link>
+      <router-link tag="li" class="card-item" to="/choose">帮你选</router-link>
+      <!-- <router-link tag="li" class="card-item" to="/helloWorld">helloWord</router-link> -->
+    </ul>
+  </div>
+</template>
