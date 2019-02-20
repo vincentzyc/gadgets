9f6ea405b6afefafcde70516296b9ad8e0a2eae0
Vincentyun <vincentzyc@qq.com>
2019-02-20 11:43:18 +0800

    add weather forecast

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="home">
-    <ul class="card-list">
-      <router-link tag="li" class="card-item" to="/personalTax">个税计算器</router-link>
-      <router-link tag="li" class="card-item" to="/choose">帮你选</router-link>
-      <!-- <router-link tag="li" class="card-item" to="/helloWorld">helloWord</router-link> -->
-    </ul>
+    <div class="card-list">
+      <router-link class="card-item" to="/personalTax">个税计算器</router-link>
+      <router-link class="card-item" to="/choose">帮你选</router-link>
+      <a class="card-item" href="http://m.weathercn.com/zh/cn/tianhe-district/2332593/extended-weather-forecast/2332593?lang=zh-cn&partner=1000001070_hfaw">天气预报</a>
+      <!-- <router-link class="card-item" to="/helloWorld">helloWord</router-link> -->
+    </div>
   </div>
 </template>
