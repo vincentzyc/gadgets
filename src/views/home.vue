b00f06ff53445d25c63c3b40e5e8205c43b98179
Vincentyun <vincentzyc@qq.com>
2019-05-13 13:57:39 +0800

    新增重名查询

--- src/views/home.vue
+++ src/views/home.vue
@@ -4,6 +4,7 @@
       <router-link class="card-item" to="/personalTax">个税计算器</router-link>
       <router-link class="card-item" to="/choose">帮你选</router-link>
       <a class="card-item" href="http://m.weathercn.com/zh/cn/tianhe-district/2332593/extended-weather-forecast/2332593?lang=zh-cn&partner=1000001070_hfaw">天气预报</a>
+      <a class="card-item" href="http://gdzaj.gd.gov.cn/gdza/web/xsecx/xseqmcx.ignore?type=cmcx">重名查询</a>
       <!-- <router-link class="card-item" to="/helloWorld">helloWord</router-link> -->
     </div>
   </div>
