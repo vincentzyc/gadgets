dfb8f57e02d6572f660e495bb26d0b13ec5657d5
Vincentyun <vincentzyc@qq.com>
2020-06-15 09:48:23 +0800

    gitHub pages

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,7 +3,7 @@
     <div class="card-list">
       <router-link class="card-item" to="/personalTax">个税计算器</router-link>
       <router-link class="card-item" to="/choose">帮你选</router-link>
-      <a class="card-item" href="http://m.weathercn.com/zh/cn/tianhe-district/2332593/extended-weather-forecast/2332593?lang=zh-cn&partner=1000001070_hfaw">天气预报</a>
+      <a class="card-item" href="https://m.weathercn.com/current-weather.do?partner=1000001071_hfaw&language=zh-cn&id=2332593">天气预报</a>
       <a class="card-item" href="http://gdzaj.gd.gov.cn/gdza/web/xsecx/xseqmcx.ignore?type=cmcx">重名查询</a>
       <!-- <router-link class="card-item" to="/helloWorld">helloWord</router-link> -->
     </div>
